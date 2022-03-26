ac863fa9b4434114b48a4e20915cf79ad290d982
Erin Scudder <mddscudder44@gmail.com>
2022-03-25 21:47:18 -0400

    Added confirm to take off button, changed background color, & shuttle height

--- scripts.js
+++ scripts.js
@@ -1,2 +1,24 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener('load', function() {
+    let takeOffBtn = document.getElementById('takeoff');
+    let flightStatus = document.getElementById('flightStatus');
+    let shuttleBackground = document.getElementById('shuttleBackground');
+    let shuttleHeight = document.getElementById('spaceShuttleHeight')
+
+
+
+
+
+    takeOffBtn.addEventListener('click', function() {
+        let confirmTakeoff = confirm("Please confirm that the shuttle is ready for takeoff")
+        if (confirmTakeoff) {
+            flightStatus.innerText = "Shuttle in flight"
+            shuttleBackground.style.backgroundColor = "blue"
+            shuttleHeight.innerHTML = 10000
+        } else {
+            console.log("its false")
+        }
+    });
+        
+})
